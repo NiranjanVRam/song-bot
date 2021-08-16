fb3e450aafb2358c2cd9b7046f7ee784f2c42a8d
NiranjanVRam <83854040+NiranjanVRam@users.noreply.github.com>
2021-08-16 23:35:00 +0530

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -14,7 +14,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 BUTTON1="𝐅𝐇 𝐌𝐔𝐒𝐈𝐂𝐒🎶"
 B2="telegram.dog/liqwid_x"
-OWNER="t.me/fhmusics"
+OWNR="t.me/fhmusics"
 GITCLONE="https://t.me/fhmusics"
 ABS="Developer"
 APPER="@fhmusics"
@@ -27,7 +27,7 @@ async def start(client, message):
                 [
                     InlineKeyboardButton(BUTTON1, url=GITCLONE)
                  ],[
-                    InlineKeyboardButton(OWNER, url=OWNER),
+                    InlineKeyboardButton(OWNER, url=OWNR),
                     InlineKeyboardButton(ABS, url=B2)
             ]
           ]
