d6d9f155a5a2baf4fb8de55db29c1e69a69d8df8
Niranjan V Ram <83854040+NiranjanVRam@users.noreply.github.com>
2021-10-24 23:25:14 +0530

    Updated start image

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name, I can help you with uploading that to TG")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
-    OWNER = os.environ.get("OWNER", "shamilhabeeb") 
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/040c516a381c87c6753a0.jpg")
+    OWNER = os.environ.get("OWNER", "liqwid_x") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}
